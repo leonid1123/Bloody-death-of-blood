f4022d931aa6e5eb73f1da03121685667a21aa83
Leo <47742726+leonid1123@users.noreply.github.com>
2022-04-12 11:50:17 +0300

    Selector troubles

--- Assets/Scripts/selector.cs
+++ Assets/Scripts/selector.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class selector : MonoBehaviour {
-
-    [SerializeField]
-    GameObject sel;
-
-    private void Start() {
-        sel.SetActive(false);
+    GameObject[] sel;
+    private void Awake() {
+        sel = GameObject.FindGameObjectsWithTag("LizardSpawnPointSelector");
+        foreach (GameObject s in sel) {
+            s.SetActive(false);
+        }
+        Debug.Log(sel.Length);
     }
     private void OnMouseDown() {
-        sel.SetActive(true);
+        foreach (GameObject s in sel) {
+            s.SetActive(false);
+        }
+        transform.GetChild(0).gameObject.SetActive(true);
     }
-
+// сделать нормальный механизм выбора домика
+// ВЕРУТЬСЯ к ОСТАЛЬНЫМ проблеМам!!!!
 }
